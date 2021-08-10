2e5197d29bd649472c007e7167f414ec196faa34
Mario Beltrán Alarcón <belco90@gmail.com>
2021-08-10 17:18:12 +0200

    add failing test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
-import Counter from '../'
+import { getByRole } from '@testing-library/dom';
 
-test('increments the count', () => {
-  const {getByText} = render(<Counter />)
-  const button = getByText('0')
-  fireEvent.click(button)
-  expect(button).toHaveTextContent('1')
-  fireEvent.click(button)
-  expect(button).toHaveTextContent('2')
-})
+function getExampleDOM() {
+  const root = document.createElement('div');
+
+  root.innerHTML = `
+    <ul>
+      <li>Item A</li>
+      <li>Item B</li>
+    </ul>
+  `;
+
+  return root;
+}
+
+test('should render list items', () => {
+  const container = getExampleDOM();
+  getByRole(container, 'list');
+  getByRole(container, 'listitem', { name: /item a/i });
+});
 
